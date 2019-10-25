8ad42c49759495e29052c0b3818e45e151b340d6
vidit23 <viditvineet@hotmail.com>
2019-10-25 12:12:21 -0400

    reading from a file

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import numpy as np 
+
+dbsStored = {}
+
+def readFromFile(dbName, readFromFile):
+    readDB = np.genfromtxt(readFromFile, dtype = None, delimiter = ',', names=True, autostrip=True)
+    dbsStored[dbName] = readDB
+    
+
 query = input('Enter your query: ')
 print('Running query: ', query)
+readFromFile("R1", "myfile.csv")
