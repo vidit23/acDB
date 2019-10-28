e247db2d610e547b18e27475c470fa50732ee0f0
vidit23 <viditvineet@hotmail.com>
2019-10-28 17:41:56 -0400

    Average function

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import numpy as np 
+import warnings
+warnings.filterwarnings("ignore", category = np.VisibleDeprecationWarning) 
 
-dbsStored = {}
+allDB = {}
 
 def readFromFile(dbName, readFromFile):
-    readDB = np.genfromtxt(readFromFile, dtype = None, delimiter = ',', names=True, autostrip=True)
-    dbsStored[dbName] = readDB
+    readDB = np.genfromtxt(readFromFile, dtype = None, delimiter = '|', names=True, autostrip=True)
+    allDB[dbName] = readDB
+
+def findAverage(dbName, column):
+    col = allDB[dbName][column] 
+    mean = np.mean(col)
+    print(mean)
+    print("The Mean for the column " + column + " is " + str(mean))
     
+
 
 query = input('Enter your query: ')
 print('Running query: ', query)
 readFromFile("R1", "myfile.csv")
+findAverage("R1", "age")
