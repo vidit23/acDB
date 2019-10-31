6701d1706a05c2022dce4db9fd84070e71c9a96b
vidit23 <viditvineet@hotmail.com>
2019-10-31 14:05:04 -0400

    max, sum, pretty print, sort functionality

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import numpy as np 
+from tabulate import tabulate
 import warnings
 warnings.filterwarnings("ignore", category = np.VisibleDeprecationWarning) 
 
-allDB = {}
+allCollections = {}
 
-def readFromFile(dbName, readFromFile):
+def printData(collection):
+    headers = collection.dtype.names
+    table = tabulate(collection, headers, tablefmt="grid")
+    print(table)
+
+def readFromFile(collectionName, readFromFile):
     readDB = np.genfromtxt(readFromFile, dtype = None, delimiter = '|', names=True, autostrip=True)
-    allDB[dbName] = readDB
+    allCollections[collectionName] = readDB
 
-def findAverage(dbName, column):
-    col = allDB[dbName][column] 
+def findAverage(collectionName, column):
+    col = allCollections[collectionName][column] 
     mean = np.mean(col)
-    print(mean)
     print("The Mean for the column " + column + " is " + str(mean))
-    
 
+def findMax(collectionName, column):
+    col = allCollections[collectionName][column] 
+    colMax = np.max(col)
+    print("The Maximum for the column " + column + " is " + str(colMax))
+
+def findSum(collectionName, column):
+    col = allCollections[collectionName][column] 
+    colSum = np.sum(col)
+    print("The Sum for the column " + column + " is " + str(colSum))
+
+def sortCollection(collectionName, column, sortedCollectionName):
+    sortedCollection = np.sort(allCollections[collectionName], order=column)
+    allCollections[sortedCollectionName] = sortedCollection
+
+# groupby function 
+# n = np.unique(a[:,0])
+# np.array( [ list(a[a[:,0]==i,1]) for i in n] )
 
 query = input('Enter your query: ')
 print('Running query: ', query)
