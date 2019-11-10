30e863061f556ff89e72b85708104678ead5d8ca
vidit23 <viditvineet@hotmail.com>
2019-11-09 23:47:54 -0500

    added calls to all functions + hashing

--- engine.py
+++ engine.py
@@ -1,22 +1,30 @@
+from collections import defaultdict
 import numpy as np
 from tabulate import tabulate
+
 import warnings
 warnings.filterwarnings("ignore", category = np.VisibleDeprecationWarning)
 
 allCollections = {}
-
-def readFromFile(outputCollection, readFromFile):
-    collection = np.genfromtxt(readFromFile, dtype = None, delimiter = '|', names = True, autostrip = True)
-    allCollections[outputCollection] = collection
+hashedKeys = {}
+btreedKeys = {}
 
 def printTable(collectionName):
     headers = collectionName.dtype.names
     table = tabulate(collectionName, headers, tablefmt = "grid")
     print(table)
 
+def readFromFile(outputCollection, readFromFile):
+    collection = np.genfromtxt(readFromFile, dtype = None, delimiter = '|', names = True, autostrip = True)
+    allCollections[outputCollection] = collection
+
 def project(outputCollection, collectionName, fields):
     allCollections[outputCollection] = allCollections[collectionName][fields]
     printTable(allCollections[outputCollection])
+
+def sortCollection(outputCollection, collectionName, column):
+    sortedCollection = np.sort(allCollections[collectionName], order=column)
+    allCollections[outputCollection] = sortedCollection
 
 def select(outputCollection, collectionName, conditions, operator):
     allResults = []
@@ -42,21 +50,28 @@ def select(outputCollection, collectionName, conditions, operator):
     elif operator == 'and':
         allCollections[outputCollection] = np.all(allResults, axis = 0)
 
-def findAverage(collectionName, column):
+def findAverage(outputCollection, collectionName, column):
     col = allCollections[collectionName][column] 
     mean = np.mean(col)
     print("The Mean for the column " + column + " is " + str(mean))
+    allCollections[outputCollection] = np.array([(mean)], dtype=[('mean(' + column + ')', 'float_')])
 
-def findMax(collectionName, column):
+def findMax(outputCollection, collectionName, column):
     col = allCollections[collectionName][column] 
     colMax = np.max(col)
+    allCollections[outputCollection] = np.array([(colMax)], dtype=[('max(' + column + ')', col.dtype)])
     print("The Maximum for the column " + column + " is " + str(colMax))
 
-def findSum(collectionName, column):
+def findSum(outputCollection, collectionName, column):
     col = allCollections[collectionName][column] 
     colSum = np.sum(col)
+    allCollections[outputCollection] = np.array([(colSum)], dtype=[('sum(' + column + ')', col.dtype)])
     print("The Sum for the column " + column + " is " + str(colSum))
 
-def sortCollection(collectionName, column, sortedCollectionName):
-    sortedCollection = np.sort(allCollections[collectionName], order=column)
-    allCollections[sortedCollectionName] = sortedCollection
+def createHash(collectionName, column):
+    hashmap = defaultdict(lambda: [])
+    collection = allCollections[collectionName]
+    for row in range(len(collection)):
+        hashmap[collection[row][column]].append(row)
+    hashedKeys[collectionName + '.' + col] = hashmap
+    print('Created a hashmap on ' + column + 'for collection ' + collectionName)
--- main.py
+++ main.py
@@ -4,10 +4,30 @@ import engine
 import numpy as np
 
 def functionalityChooser(queryMeaning):
-    if queryMeaning["functionName"] == "inputfromfile":
-        engine.readFromFile(queryMeaning["outputDB"], queryMeaning["input"])
-    elif queryMeaning["functionName"] == "project":
-        engine.project(queryMeaning["outputDB"], queryMeaning["input"], queryMeaning["fields"])
+    if queryMeaning['functionName'] == 'inputfromfile':
+        # Read from a file
+        engine.readFromFile(queryMeaning['outputDB'], queryMeaning['input'])
+    elif queryMeaning['functionName'] == 'project':
+        # Select a particular set of columns from a collection
+        engine.project(queryMeaning['outputDB'], queryMeaning['input'], queryMeaning['fields'])
+    elif queryMeaning['functionName'] == 'sort':
+        # Sort the collection based on a set of columns
+        engine.sortCollection(queryMeaning['outputDB'], queryMeaning['input'], queryMeaning['fields'])
+    elif queryMeaning['functionName'] == 'select':
+        # Select rows from a collection based on some conditions
+        engine.select(queryMeaning['outputDB'], queryMeaning['input'], queryMeaning['fields'], queryMeaning['condition'])
+    elif queryMeaning['functionName'] == 'avg':
+        # Find the average value of a column
+        engine.findAverage(queryMeaning['outputDB'], queryMeaning['input'], queryMeaning['fields'])
+    elif queryMeaning['functionName'] == 'max':
+        # Find the Maximum value of a column
+        engine.findMax(queryMeaning['outputDB'], queryMeaning['input'], queryMeaning['fields'])
+    elif queryMeaning['functionName'] == 'sum':
+        # Find the sum of all the values in a column
+        engine.findSum(queryMeaning['outputDB'], queryMeaning['input'], queryMeaning['fields'])
+    elif queryMeaning['functionName'] == 'Hash':
+        # Create a hashmap for a particular column
+        engine.createHash(queryMeaning['input'], queryMeaning['fields'])
 
 # groupby function 
 # n = np.unique(a[:,0])
@@ -20,14 +40,26 @@ while 1:
         break
     queryMeaning = parser(query)
     print('Extracted meaning: ', queryMeaning)
-    functionalityChooser({"outputDB": 'R', 
-                        "functionName": 'inputfromfile', 
-                        "input":'myfile.csv',
-                        "fields": None,
-                        "condition": None })
+    functionalityChooser({'outputDB': 'R', 
+                        'functionName': 'inputfromfile', 
+                        'input':'myfile.csv',
+                        'fields': None,
+                        'condition': None })
     
-    functionalityChooser({"outputDB": 'R1', 
-                        "functionName": 'project', 
-                        "input":'R',
-                        "fields": ['id', 'name'],
-                        "condition": None })
+    functionalityChooser({'outputDB': 'R1', 
+                        'functionName': 'project', 
+                        'input':'R',
+                        'fields': ['id', 'name'],
+                        'condition': None })
+    
+    functionalityChooser({'outputDB': 'R3', 
+                        'functionName': 'select', 
+                        'input':'R',
+                        'fields': [[],[],[]],
+                        'condition': 'or' })
+
+    functionalityChooser({'outputDB': 'R1', 
+                        'functionName': 'project', 
+                        'input':'R',
+                        'fields': ['id', 'name'],
+                        'condition': None })
